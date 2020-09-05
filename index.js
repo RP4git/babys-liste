d31dd2aa2dc6bde4fe3f483db981b375ec1c043b
Karen Fuchs <kfuchs@irt.de>
2020-09-05 15:26:16 +0200

    added first functionality, next step: first populating

--- index.js
+++ index.js
@@ -1,32 +1,107 @@
 Vue.component('list-comp', {
-  props: ['category', 'entries'],
+  props: ['category', 'entries', 'catindex'],
   template:
-    '  <ul>    <li class="d-flex justify-content-between align-items-center">  <button type="button" class="btn btn-outline-secondary" >{{category.text}}</button>    <span class="badge badge-primary badge-pill">{{category.items}}</span></li>    <li style="list-style-type:none;">     <ul style="list-style-type:none;" v-show=category.show>   <li v-for="(entry, index) in entries" :entry="entry" :key="index"><test-comp :entry="entry"></test-comp></li>      </ul></li></ul>',
+    '  <ul>    <li class="d-flex justify-content-between align-items-center">  <button type="button" class="btn btn-outline-secondary" :disabled="category.items<=0" @click="showEntries(catindex)">{{category.text}}</button>    <span class="badge badge-primary badge-pill">{{category.items}}</span></li>    <li style="list-style-type:none;">     <ul style="list-style-type:none;" v-show=category.show>   <li><test-comp :entry="entries[[catindex]]" :entryNo="catindex" @toggle-check="toggleLinkState"></test-comp></li>      </ul></li></ul>',
+  data() {
+    return {
+      disabledLinks: [],
+    };
+  },
+  methods: {
+    showEntries: function (catindex) {
+      this.$emit('show-entries', catindex);
+    },
+    toggleLinkState(index) {
+      this.$emit('toggle-link-state', index);
+    },
+  },
 });
 
 Vue.component('test-comp', {
-  props: ['entry'],
-  template: '<span v-html="entry"></span>',
+  props: ['entry', 'entryNo'],
+  template:
+    '<ul style="list-style-type:none;">   <li v-for="(item, index) in entry.entrylist" :key="index"> <input type="checkbox" :id="index" @change="toggleCheck([entryNo,index])"><label :for="index" style="margin-left: .8%;"><span v-html="item"></span></label> </li></ul>',
+  methods: {
+    toggleCheck: function (index) {
+      this.$emit('toggle-check', index);
+    },
+  },
 });
-// <li> v-for="(item,index) in entries"    :entries="item"    :key="index">{{{item}}}  </li>
 
 var app = new Vue({
   el: '#app',
   data: {
     categories: [
-      { items: 0, text: 'Reisen', show: true, link: '' },
-      { items: 1, text: 'Haushalt und Technik', show: true },
-      { items: 2, text: 'babysichere Wohnung', show: false },
-      { items: 2, text: 'aus dem Möbelhaus', show: false },
-      { items: 2, text: 'Babypflege', show: false },
-      { items: 2, text: 'Babytextilien', show: false },
-      { items: 0, text: 'Babys Ernährung', show: false },
-      { items: 0, text: 'Babys erstes Spielzeug', show: false },
+      { items: 3, text: 'Reisen', show: true, link: '' },
+      { items: 2, text: 'Haushalt und Technik', show: false },
+      { items: 0, text: 'babysichere Wohnung', show: false },
+      { items: 3, text: 'aus dem Möbelhaus', show: false },
+      { items: 3, text: 'Babypflege', show: false },
+      { items: 3, text: 'Babytextilien', show: false },
+      { items: 3, text: 'Babys Ernährung', show: false },
+      { items: 3, text: 'Babys erstes Spielzeug', show: false },
     ],
     entries: [
-      '<a href="none" rel="none" target="_blank">        Reisennnnn      </a>',
-      '<a href="none" rel="none" target="_blank">           Reisen2          </a>',
-      'sss',
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        Reisennnnn      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           Reisen2          </a>',
+          'sss',
+        ],
+      },
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        haushalt1      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           haushalt2          </a>',
+        ],
+      },
+      {
+        entrylist: [],
+      },
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        Reisennnnn      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           Reisen2          </a>',
+          'sss',
+        ],
+      },
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        Reisennnnn      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           Reisen2          </a>',
+          'sss',
+        ],
+      },
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        Reisennnnn      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           Reisen2          </a>',
+          'sss',
+        ],
+      },
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        Reisennnnn      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           Reisen2          </a>',
+          'sss',
+        ],
+      },
+      {
+        entrylist: [
+          '<a href="https://www.google.de/" re="noreferrer noopener" target="_blank">        Reisennnnn      </a>',
+          '<a href="none" re="noreferrer noopener" target="_blank">           Reisen2          </a>',
+          'sss',
+        ],
+      },
     ],
+  },
+  methods: {
+    showAllEntries(catindex) {
+      this.categories[[catindex]].show = !this.categories[[catindex]].show;
+    },
+    toggleLink(index) {
+      console.log(index);
+      console.log(this.entries[[index[0]]].entrylist[index[1]]);
+    },
   },
 });
