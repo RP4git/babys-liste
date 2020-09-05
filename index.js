c74d93dcfd9226da9b9206496389c856fdb64584
Karen Fuchs <kfuchs@irt.de>
2020-09-05 12:39:35 +0200

    links fixed, next step: first populating

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 Vue.component('list-comp', {
-  props: ['categories', 'entries'],
+  props: ['category', 'entries'],
   template:
-    '  <ul>    <li class="d-flex justify-content-between align-items-center">  <button type="button" class="btn btn-outline-secondary" >{{categories.text}}</button>    <span class="badge badge-primary badge-pill">{{categories.items}}</span></li>    <li>     <ul style="list-style-type:none;" v-show=categories.show>   <li v-for="item in entries"    v-bind:entries="item"    v-bind:key="item.id">item.entry  </li>      </ul></ul>',
+    '  <ul>    <li class="d-flex justify-content-between align-items-center">  <button type="button" class="btn btn-outline-secondary" >{{category.text}}</button>    <span class="badge badge-primary badge-pill">{{category.items}}</span></li>    <li style="list-style-type:none;">     <ul style="list-style-type:none;" v-show=category.show>   <li v-for="(entry, index) in entries" :entry="entry" :key="index"><test-comp :entry="entry"></test-comp></li>      </ul></li></ul>',
 });
+
+Vue.component('test-comp', {
+  props: ['entry'],
+  template: '<span v-html="entry"></span>',
+});
+// <li> v-for="(item,index) in entries"    :entries="item"    :key="index">{{{item}}}  </li>
 
 var app = new Vue({
   el: '#app',
   data: {
     categories: [
-      { items: 0, text: 'Reisen', show: true },
+      { items: 0, text: 'Reisen', show: true, link: '' },
       { items: 1, text: 'Haushalt und Technik', show: true },
       { items: 2, text: 'babysichere Wohnung', show: false },
       { items: 2, text: 'aus dem Möbelhaus', show: false },
@@ -18,21 +24,9 @@ var app = new Vue({
       { items: 0, text: 'Babys erstes Spielzeug', show: false },
     ],
     entries: [
-      // {
-      //   entry: (
-      //     <a href="none" rel="none" target="_blank">
-      //       Reisen
-      //     </a>
-      //   ),
-      // },
-      // {
-      //   entry: (
-      //     <a href="none" rel="none" target="_blank">
-      //       Reisen2
-      //     </a>
-      //   ),
-      // },
-      { entry: 'sss' },
+      '<a href="none" rel="none" target="_blank">        Reisennnnn      </a>',
+      '<a href="none" rel="none" target="_blank">           Reisen2          </a>',
+      'sss',
     ],
   },
 });
